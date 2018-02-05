3824567f1f6b506232156d7f81a29833b09d6488
tvm-thomson <thomson.varkey@thrithvam.me>
2018-02-05 17:26:34 +0530

    Added dashboard page and admin dashboard

---
+++ ProductionApp.UserInterface/Controllers/PurchaseDashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductionApp.UserInterface.Controllers
+{
+    public class PurchaseDashboardController : Controller
+    {
+        // GET: PurchaseDashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
