9fdad39e5d86e7f044a8f5f329731e1a39b23eec
Angel Paul <Angel Paul@DESKTOP-DCRUNKI>
2018-02-14 13:16:55 +0530

    ViewPurchaseOrder

---
+++ ProductionApp.UserInterface/Controllers/PurchaseOrderController.cs
@@ -0,0 +1,136 @@
+﻿using AutoMapper;
+using Newtonsoft.Json;
+using ProductionApp.BusinessService.Contracts;
+using ProductionApp.DataAccessObject.DTO;
+using ProductionApp.UserInterface.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductionApp.UserInterface.Controllers
+{
+    public class PurchaseOrderController : Controller
+    {
+        // GET: PurchaseOrder
+        private IPurchaseOrderBusiness _purchaseOrderBusiness;
+        Common _common = new Common();
+        public PurchaseOrderController(IPurchaseOrderBusiness purchaseOrderBusiness)
+        {
+            _purchaseOrderBusiness = purchaseOrderBusiness;
+        }
+        public ActionResult ViewPurchaseOrder(string code)
+        {
+            ViewBag.SysModuleCode = code;
+            //ViewBag.toDate = (_common.GetCurrentDateTime()).ToString("dd-MMM-yyyy");
+            //ViewBag.fromDate=_common.
+            PurchaseOrderAdvanceSearchViewModel purchaseOrderAdvanceSearchVM = new PurchaseOrderAdvanceSearchViewModel();
+            List<SelectListItem> selectListItem = new List<SelectListItem>();
+            selectListItem = new List<SelectListItem>();
+            selectListItem.Add(new SelectListItem { Text = "Open", Value = "Open", Selected = true });
+            selectListItem.Add(new SelectListItem { Text = "Closed", Value = "Closed", Selected = false });
+            selectListItem.Add(new SelectListItem { Text = "All", Value = "ALL", Selected = false });
+            purchaseOrderAdvanceSearchVM.Status = selectListItem;
+            return View(purchaseOrderAdvanceSearchVM);
+        }
+        #region GetAllPurchaseOrder
+        [HttpPost]
+        public JsonResult GetAllPurchaseOrder(DataTableAjaxPostModel model, PurchaseOrderAdvanceSearchViewModel purchaseOrderAdvanceSearchVM)
+        {
+            purchaseOrderAdvanceSearchVM.DataTablePaging.Start = model.start;
+            purchaseOrderAdvanceSearchVM.DataTablePaging.Length = (purchaseOrderAdvanceSearchVM.DataTablePaging.Length == 0 ? model.length : purchaseOrderAdvanceSearchVM.DataTablePaging.Length);
+            List<PurchaseOrderHeaderViewModel> purchaseOrderList = Mapper.Map<List<PurchaseOrderHeader>, List<PurchaseOrderHeaderViewModel>>(_purchaseOrderBusiness.GetAllPurchaseOrder(Mapper.Map<PurchaseOrderAdvanceSearchViewModel, PurchaseOrderAdvanceSearch>(purchaseOrderAdvanceSearchVM)));
+            var settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.None
+            };
+            return Json(new
+            {
+                draw = model.draw,
+            recordsTotal = purchaseOrderList.Count != 0 ? purchaseOrderList[0].TotalCount : 0,
+            recordsFiltered = purchaseOrderList.Count != 0 ? purchaseOrderList[0].FilteredCount : 0,
+            data= purchaseOrderList
+            
+        });
+        }
+        #endregion GetAllPurchaseOrder
+        #region ButtonStyling
+        [HttpGet]
+        //[AuthSecurityFilter(ProjectObject = "Bank", Mode = "R")]
+        public ActionResult ChangeButtonStyle(string actionType)
+        {
+            ToolboxViewModel toolboxVM = new ToolboxViewModel();
+            switch (actionType)
+            {
+                case "List":
+                    toolboxVM.addbtn.Visible = true;
+                    toolboxVM.addbtn.Text = "Add";
+                    toolboxVM.addbtn.Title = "Add New";
+                    toolboxVM.addbtn.Href = Url.Action("AddPurchaseOrder", "PurchaseOrder", new { code = "PURCH" });
+                    toolboxVM.addbtn.Event = "";
+                    //----added for reset button---------------
+                    toolboxVM.resetbtn.Visible = true;
+                    toolboxVM.resetbtn.Text = "Reset";
+                    toolboxVM.resetbtn.Title = "Reset All";
+                    toolboxVM.resetbtn.Event = "ResetPurchaseOrderList();";
+                    //----added for export button--------------
+                    toolboxVM.PrintBtn.Visible = true;
+                    toolboxVM.PrintBtn.Text = "Export";
+                    toolboxVM.PrintBtn.Title = "Export";
+                    toolboxVM.PrintBtn.Event = "ImportPurchaseOrderData();";
+                    //---------------------------------------
+
+                    break;
+                case "Edit":
+
+                    toolboxVM.addbtn.Visible = true;
+                    toolboxVM.addbtn.Text = "New";
+                    toolboxVM.addbtn.Title = "Add New";
+                    toolboxVM.addbtn.Event = "openNav();";
+
+                    toolboxVM.savebtn.Visible = true;
+                    toolboxVM.savebtn.Text = "Save";
+                    toolboxVM.savebtn.Title = "Save Bank";
+                    toolboxVM.savebtn.Event = "Save();";
+
+                    toolboxVM.deletebtn.Visible = true;
+                    toolboxVM.deletebtn.Text = "Delete";
+                    toolboxVM.deletebtn.Title = "Delete Bank";
+                    toolboxVM.deletebtn.Event = "Delete()";
+
+                    toolboxVM.resetbtn.Visible = true;
+                    toolboxVM.resetbtn.Text = "Reset";
+                    toolboxVM.resetbtn.Title = "Reset";
+                    toolboxVM.resetbtn.Event = "Reset();";
+
+                    toolboxVM.CloseBtn.Visible = true;
+                    toolboxVM.CloseBtn.Text = "Close";
+                    toolboxVM.CloseBtn.Title = "Close";
+                    toolboxVM.CloseBtn.Event = "closeNav();";
+
+                    break;
+                case "Add":
+
+                    toolboxVM.savebtn.Visible = true;
+                    toolboxVM.savebtn.Text = "Save";
+                    toolboxVM.savebtn.Title = "Save";
+                    toolboxVM.savebtn.Event = "Save();";
+
+                    toolboxVM.ListBtn.Visible = true;
+                    toolboxVM.ListBtn.Text = "List";
+                    toolboxVM.ListBtn.Title = "List";
+                    toolboxVM.ListBtn.Event = "";
+                    toolboxVM.ListBtn.Href = Url.Action("ViewPurchaseOrder", "PurchaseOrder", new { Code = "PURCH" });
+
+
+                    break;
+                default:
+                    return Content("Nochange");
+            }
+            return PartialView("ToolboxView", toolboxVM);
+        }
+
+        #endregion
+    }
+}
