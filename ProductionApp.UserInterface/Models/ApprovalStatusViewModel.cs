8fc7b84bd36831600942345636006984a52ef8ba
Arul Vyas <Arul Vyas@DESKTOP-G0M3RCR>
2018-02-15 12:39:47 +0530

    ApprovalLog,ApprovalStatus - DTO,ViewModel [Added]

---
+++ ProductionApp.UserInterface/Models/ApprovalStatusViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductionApp.UserInterface.Models
+{
+    public class ApprovalStatusViewModel
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public CommonViewModel Common { get; set; }
+    }
+}
