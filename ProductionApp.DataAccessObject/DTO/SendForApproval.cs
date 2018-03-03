8ac48178146fd1d6a9b8748b0f32233df5dd1abd
GibinJacob <gibin.jacob.job@gmail.com>
2018-03-03 14:50:41 +0530

    send for approval partial view files added

---
+++ ProductionApp.DataAccessObject/DTO/SendForApproval.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionApp.DataAccessObject.DTO
+{
+    public class SendForApproval
+    {
+        public Guid? ApproverID { get; set; }
+        public Guid? UserID { get; set; }
+        public string UserName { get; set; }
+        public string EmailID { get; set; }
+        public int ApproverLevel { get; set; }
+        public string ApproverCSV { get; set; }
+    }
+}
