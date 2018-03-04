00264cb656df6b178ecba94e91ff938fd853ec0f
Suvaneeth <suvaneeth.thrithvam@outlook.com>
2018-03-04 17:10:55 +0530

    graph added

--- ProductionApp.UserInterface/Scripts/UserJS/Home/SalesSummary.js
+++ ProductionApp.UserInterface/Scripts/UserJS/Home/SalesSummary.js
@@ -1,23 +1,103 @@
 ﻿function CreateSalesSummary() {
-    var chrt = document.getElementById("myChart").getContext("2d");
+    var chrt = document.getElementById("saleschart").getContext("2d");
     var myarr = [65, 59, 80, 81, 56, 55, 40];
     var data = {
         labels: lbls, //x-axis
         datasets: [
         {
-            label: "My First dataset", //optional
-            fillColor: "rgba(220,220,220,0.8)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
+            legendText: "Sales Months",
+            label: "Sales months", //optional
+            fillColor: "rgba(100,220,220,0.8)",
+            strokeColor: "rgba(100,220,220,0.8)",
+            highlightFill: "rgba(100,220,220,0.75)",
+            highlightStroke: "rgba(100,220,220,1)",
             data: dta // y-axis
-        } 
+        }
         ]
+
     };
 
-    var myFirstChart = new Chart(chrt).Bar(data);
+    options = {
+
+        //Boolean - If we show the scale above the chart data			
+        scaleOverlay: false,
+
+        //Boolean - If we want to override with a hard coded scale
+        scaleOverride: false,
+
+        //** Required if scaleOverride is true **
+        //Number - The number of steps in a hard coded scale
+        scaleSteps: null,
+        //Number - The value jump in the hard coded scale
+        scaleStepWidth: null,
+        //Number - The scale starting value
+        scaleStartValue: null,
+
+        //String - Colour of the scale line	
+        scaleLineColor: "rgba(0,0,0,.1)",
+
+        //Number - Pixel width of the scale line	
+        scaleLineWidth: 1,
+
+        //Boolean - Whether to show labels on the scale	
+        scaleShowLabels: true,
+
+        //Interpolated JS string - can access value
+        scaleLabel: "<%=value%>",
+
+        //String - Scale label font declaration for the scale label
+        scaleFontFamily: "'Arial'",
+
+        //Number - Scale label font size in pixels	
+        scaleFontSize: 12,
+
+        //String - Scale label font weight style	
+        scaleFontStyle: "normal",
+
+        //String - Scale label font colour	
+        scaleFontColor: "#666",
+
+        ///Boolean - Whether grid lines are shown across the chart
+        scaleShowGridLines: true,
+
+        //String - Colour of the grid lines
+        scaleGridLineColor: "rgba(0,0,0,.05)",
+
+        //Number - Width of the grid lines
+        scaleGridLineWidth: 1,
+
+        //Boolean - If there is a stroke on each bar	
+        barShowStroke: true,
+
+        //Number - Pixel width of the bar stroke	
+        barStrokeWidth: 2,
+
+        //Number - Spacing between each of the X value sets
+        barValueSpacing: 5,
+
+        //Number - Spacing between data sets within X values
+        barDatasetSpacing: 1,
+
+        //Boolean - Whether to animate the chart
+        animation: true,
+
+        //Number - Number of animation steps
+        animationSteps: 60,
+
+        //String - Animation easing effect
+        animationEasing: "easeOutQuart",
+
+        //Function - Fires when the animation is complete
+        onAnimationComplete: null,
+
+        tooltipTemplate: '<%=label%> : ₹ <%=value %> Lakhs'
+
+    }
+
+
+    var saleschart = new Chart(chrt).Bar(data, options);
+    
 }
- 
 
 $(function() {
     CreateSalesSummary();
