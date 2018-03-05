4501b46cb6e430ebd2b174e14bc87f7ad389e98a
Suvaneeth <suvaneeth.thrithvam@outlook.com>
2018-03-05 19:34:45 +0530

    summary invoice

--- ProductionApp.UserInterface/Scripts/UserJS/Home/IncomeExpenseSummary.js
+++ ProductionApp.UserInterface/Scripts/UserJS/Home/IncomeExpenseSummary.js
@@ -4,6 +4,22 @@
 
 });
 
+
+Chart.types.Line.extend({
+    name: "LineAlt",
+    initialize: function (data) {
+        Chart.types.Line.prototype.initialize.apply(this, arguments);
+        this.eachPoints(function (point, index) {
+            Chart.helpers.extend(point, {
+                x: this.scale.calculateX(0),
+                y: this.scale.calculateY(point.value)
+            });
+            point.save();
+        }, this);
+    }
+});
+
+
 function IncomeExpenseChart() {
 
     var list = IncomeExpense;
