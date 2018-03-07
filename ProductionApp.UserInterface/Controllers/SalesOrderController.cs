9c49260b2179299b5608b1e249c4ffc064bdca0f
GibinJacob <gibin.jacob.job@gmail.com>
2018-03-07 12:06:58 +0530

    Sales Order New files Added

---
+++ ProductionApp.UserInterface/Controllers/SalesOrderController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductionApp.UserInterface.Controllers
+{
+    public class SalesOrderController : Controller
+    {
+        // GET: SalesOrder
+        public ActionResult AddSalesOrder(string code, Guid? id)
+        {
+            ViewBag.SysModuleCode = code;
+
+            return View();
+        }
+        public ActionResult ListSalesOrder(string code)
+        {
+            ViewBag.SysModuleCode = code;
+
+            return View();
+        }
+    }
+}
