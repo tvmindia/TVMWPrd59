973d68c9c945b2637261f47d7cddebc64a91dd0d
GibinJacob <gibin.jacob.job@gmail.com>
2018-03-07 12:53:25 +0530

    DTO,Viewmodel Added: Sales Order

---
+++ ProductionApp.DataAccessObject/DTO/SalesOrder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionApp.DataAccessObject.DTO
+{
+    class SalesOrder
+    {
+        public Guid ID { get; set; }
+        public string OrderNo { get; set; }
+        public DateTime OrderDate { get; set; }
+        public Guid CustomerID { get; set; }
+        public Guid SalesPerson { get; set; }
+        public DateTime ExpectedDeliveryDate { get; set; }
+        public string BillingAddress { get; set; }
+        public string ShippingAddress { get; set; }
+        public string Remarks { get; set; }
+        
+        //additional properties
+        public string OrderDateFormatted { get; set; }
+        public string ExpectedDeliveryDateFormatted { get; set; }
+        public Common Common { get; set; }
+        public List<SalesOrderDetail> SalesOrderDetailList { get; set; }
+
+
+    }
+    public class SalesOrderDetail
+    {
+        public Guid ID { get; set; }
+        public Guid SalesOrderID { get; set; }
+        public Guid ProductID { get; set; }
+        public string TaxTypeCode { get; set; }
+        public decimal Quantity { get; set; }
+        public string UnitCode { get; set; }
+        public decimal Rate { get; set; }
+        public decimal TradeDiscountAmount { get; set; }
+        public DateTime ExpectedDeliveryDate { get; set; }
+
+        //additional properties
+        public string ExpectedDeliveryDateFormatted { get; set; }
+        public Common Common { get; set; }
+
+    }
+}
---
+++ ProductionApp.UserInterface/Models/SalesOrderViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductionApp.UserInterface.Models
+{
+    public class SalesOrderViewModel
+    {
+        public Guid ID { get; set; }
+        public string OrderNo { get; set; }
+        public DateTime OrderDate { get; set; }
+        public Guid CustomerID { get; set; }
+        public Guid SalesPerson { get; set; }
+        public DateTime ExpectedDeliveryDate { get; set; }
+        public string BillingAddress { get; set; }
+        public string ShippingAddress { get; set; }
+        public string Remarks { get; set; }
+
+        //additional properties
+        public string OrderDateFormatted { get; set; }
+        public string ExpectedDeliveryDateFormatted { get; set; }
+        public CommonViewModel Common { get; set; }
+        public List<SalesOrderDetailViewModel> SalesOrderDetailList { get; set; }
+
+
+    }
+    public class SalesOrderDetailViewModel
+    {
+        public Guid ID { get; set; }
+        public Guid SalesOrderID { get; set; }
+        public Guid ProductID { get; set; }
+        public string TaxTypeCode { get; set; }
+        public decimal Quantity { get; set; }
+        public string UnitCode { get; set; }
+        public decimal Rate { get; set; }
+        public decimal TradeDiscountAmount { get; set; }
+        public DateTime ExpectedDeliveryDate { get; set; }
+        
+        //additional properties
+        public string ExpectedDeliveryDateFormatted { get; set; }
+        public CommonViewModel Common { get; set; }
+
+    }
+}
