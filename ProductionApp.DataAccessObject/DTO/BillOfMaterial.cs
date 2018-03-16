5e444a77d4e862b6184bd5a4d060a0f27a1176fa
Arul Vyas <Arul Vyas@DESKTOP-G0M3RCR>
2018-03-16 09:33:12 +0530

    ViewModel

--- ProductionApp.DataAccessObject/DTO/BillOfMaterial.cs
+++ ProductionApp.DataAccessObject/DTO/BillOfMaterial.cs
@@ -8,24 +8,64 @@ namespace ProductionApp.DataAccessObject.DTO
 {
     public class BillOfMaterial
     {
-        Guid ID { get; set; }
-        string Description { get; set; }
-        Guid ProductID { get; set; }
-        Common Common { get; set; }
+        public Guid ID { get; set; }
+        public string Description { get; set; }
+        public Guid ProductID { get; set; }
+        public Common Common { get; set; }
+        //Additional
+        public Product Product { get; set; }
+        public List<BillOfMaterialDetail> BillOfMaterialDetailList { get; set; }
+        public int TotalCount { get; set; }
+        public int FilteredCount { get; set; }
+        public bool IsUpdate { get; set; }
+        public string DetailXML { get; set; }
+        public BOMComponentLine BOMComponentLine { get; set; }
+        public List<BOMComponentLine> BOMComponentLineList { get; set; }
+        public BOMComponentLineDetail BOMComponentLineDetail { get; set; }
+        public List<BOMComponentLineDetail> BOMComponentLineDetailList { get; set; }
     }
     public class BillOfMaterialAdvanceSearch
     {
         public DataTablePaging DataTablePaging { get; set; }
         public string SearchTerm { get; set; }
-        public string FromDate { get; set; }
-        public string ToDate { get; set; }
     }
     public class BillOfMaterialDetail
     {
-        Guid ID { get; set; }
-        Guid BillOfMaterialID { get; set; }
-        Guid ComponentID { get; set; }
-        decimal Qty { get; set; }
-        Common Common { get; set; }
+        public Guid ID { get; set; }
+        public Guid BillOfMaterialID { get; set; }
+        public Guid ComponentID { get; set; }
+        public decimal Qty { get; set; }
+        public Common Common { get; set; }
+        //Additional
+        public Product Product { get; set; }
+    }
+    public class BOMComponentLine
+    {
+        public Guid ID { get; set; }
+        public Guid ComponentID { get; set; }
+        public string LineName { get; set; }
+        public Common Common { get; set; }
+        //Additional
+        public List<BOMComponentLineStage> BOMComponentLineStage { get; set; }
+        public List<BOMComponentLineDetail> BOMComponentLineDetail { get; set; }
+    }
+    public class BOMComponentLineStage
+    {
+        public Guid ID { get; set; }
+        public Guid ComponentLineID { get; set; }
+        public Guid StageID { get; set; }
+        public int StageOrder { get; set; }
+        public Common Common { get; set; }
+    }
+    public class BOMComponentLineDetail
+    {
+        public Guid ID { get; set; }
+        public Guid ComponentLineID { get; set; }
+        public Guid StageID { get; set; }
+        public string PartType { get; set; }
+        public Guid PartID { get; set; }
+        public string EntryType { get; set; }
+        public decimal Qty { get; set; }
+        public Common Common { get; set; }
     }
 }
--- ProductionApp.UserInterface/Models/BillOfMaterialViewModel.cs
+++ ProductionApp.UserInterface/Models/BillOfMaterialViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,24 +8,72 @@ namespace ProductionApp.UserInterface.Models
 {
     public class BillOfMaterialViewModel
     {
-        Guid ID { get; set; }
-        string Description { get; set; }
-        Guid ProductID { get; set; }
-        CommonViewModel Common { get; set; }
+        public Guid ID { get; set; }
+        [Required(ErrorMessage ="Descritpion Required")]
+        public string Description { get; set; }
+        [Required(ErrorMessage ="Please select a Product")]
+        public Guid ProductID { get; set; }
+        public CommonViewModel Common { get; set; }
+        //Additional
+        [Display(Name = "Product")]
+        public ProductViewModel Product { get; set; }
+        public List<BillOfMaterialDetailViewModel> BillOfMaterialDetailList { get; set; }
+        public int TotalCount { get; set; }
+        public int FilteredCount { get; set; }
+        public bool IsUpdate { get; set; }
+        public string DetailJSON { get; set; }
+        public BOMComponentLineViewModel BOMComponentLine { get; set; }
+        public List<BOMComponentLineViewModel> BOMComponentLineList { get; set; }
+        public BOMComponentLineDetailViewModel BOMComponentLineDetail { get; set; }
+        public List<BOMComponentLineDetailViewModel> BOMComponentLineDetailList { get; set; }
     }
     public class BillOfMaterialAdvanceSearchViewModel
     {
         public DataTablePagingViewModel DataTablePaging { get; set; }
+        [Display(Name ="Search")]
         public string SearchTerm { get; set; }
-        public string FromDate { get; set; }
-        public string ToDate { get; set; }
     }
     public class BillOfMaterialDetailViewModel
     {
-        Guid ID { get; set; }
-        Guid BillOfMaterialID { get; set; }
-        Guid ComponentID { get; set; }
-        decimal Qty { get; set; }
-        CommonViewModel Common { get; set; }
+        public Guid ID { get; set; }
+        public Guid BillOfMaterialID { get; set; }
+        public Guid ComponentID { get; set; }
+        public decimal Qty { get; set; }
+        public CommonViewModel Common { get; set; }
+        //Additional
+        public ProductViewModel Product { get; set; }
+    }
+    public class BOMComponentLineViewModel
+    {
+        public Guid ID { get; set; }
+        [Display(Name = "Select Component")]
+        [Required(ErrorMessage = "Component Required")]
+        public Guid ComponentID { get; set; }
+        [Display(Name = "Production Line Name")]
+        [Required(ErrorMessage = "Line Name Required")]
+        public string LineName { get; set; }
+        public CommonViewModel Common { get; set; }
+
+        List<BOMComponentLineStageViewModel> BOMComponentLineStageList { get; set; }
+        List<BOMComponentLineDetailViewModel> BOMComponentLineDetailList { get; set; }
+    }
+    public class BOMComponentLineStageViewModel
+    {
+        public Guid ID { get; set; }
+        public Guid ComponentLineID { get; set; }
+        public Guid StageID { get; set; }
+        public int StageOrder { get; set; }
+        public CommonViewModel Common { get; set; }
+    }
+    public class BOMComponentLineDetailViewModel
+    {
+        public Guid ID { get; set; }
+        public Guid ComponentLineID { get; set; }
+        public Guid StageID { get; set; }
+        public string PartType { get; set; }
+        public Guid PartID { get; set; }
+        public string EntryType { get; set; }
+        public decimal Qty { get; set; }
+        public CommonViewModel Common { get; set; }
     }
 }
