1b18b86cdad3b3346045a0b471795f0308a87933
Arul Vyas <Arul Vyas@DESKTOP-G0M3RCR>
2018-03-16 09:30:27 +0530

    Mapping,Partial View

--- ProductionApp.UserInterface/App_Start/MappingConfig.cs
+++ ProductionApp.UserInterface/App_Start/MappingConfig.cs
@@ -80,6 +80,9 @@ namespace ProductionApp.UserInterface.App_Start
                 config.CreateMap<BillOfMaterialViewModel,BillOfMaterial>().ReverseMap();
                 config.CreateMap<BillOfMaterialDetailViewModel,BillOfMaterialDetail>().ReverseMap();
                 config.CreateMap<BillOfMaterialAdvanceSearchViewModel,BillOfMaterialAdvanceSearch>().ReverseMap();
+                config.CreateMap<BOMComponentLineViewModel, BOMComponentLine>().ReverseMap();
+                config.CreateMap<BOMComponentLineDetailViewModel, BOMComponentLineDetail>().ReverseMap();
+                config.CreateMap<BOMComponentLineStageViewModel, BOMComponentLineStage>().ReverseMap();
                 config.CreateMap<MaterialReturnFromProductionViewModel, MaterialReturnFromProduction>().ReverseMap();
                 config.CreateMap<MaterialReturnFromProductionDetailViewModel, MaterialReturnFromProductionDetail>().ReverseMap();
                 config.CreateMap<CustomerViewModel, Customer>().ReverseMap();
