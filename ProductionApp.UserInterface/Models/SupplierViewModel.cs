4cf996e606f1df997b0c96df0ad453ad549baff5
Jais <jais.thrithvam@outlook.com>
2018-03-17 14:35:27 +0530

    1.Added Customer and supplier properties

--- ProductionApp.UserInterface/Models/SupplierViewModel.cs
+++ ProductionApp.UserInterface/Models/SupplierViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,22 +10,39 @@ namespace ProductionApp.UserInterface.Models
     public class SupplierViewModel
     {
         public Guid ID { get; set; }
+        [Display(Name = "Company Name")]
         public string CompanyName { get; set; }
+        [Display(Name = "Contact Person")]
         public string ContactPerson { get; set; }
+        [Display(Name = "Email")]
         public string ContactEmail { get; set; }
+        [Display(Name = "Title")]
         public string ContactTitle { get; set; }
         public string Product { get; set; }
         public string Website { get; set; }
         public string LandLine { get; set; }
         public string Mobile { get; set; }
         public string Fax { get; set; }
+        [Display(Name = "Other Phone Nos")]
         public string OtherPhoneNos { get; set; }
+        [Display(Name = "Billing Address")]
         public string BillingAddress { get; set; }
+        [Display(Name = "Shipping Address")]
         public string ShippingAddress { get; set; }
+        [Display(Name = "Payment Term Code")]
         public string PaymentTermCode { get; set; }
+        [Display(Name = "Tax Reg No")]
         public string TaxRegNo { get; set; }
+        [Display(Name = "PAN No")]
         public string PANNo { get; set; }
+        [Display(Name = "General Notes")]
         public string GeneralNotes { get; set; }
         public List<SelectListItem> SelectList { get; set; }
+    }
+    public class SupplierAdvanceSearchViewModel
+    {
+        [Display(Name = "Search")]
+        public string SearchTerm { get; set; }
+        public DataTablePagingViewModel DataTablePaging { get; set; }
     }
 }
