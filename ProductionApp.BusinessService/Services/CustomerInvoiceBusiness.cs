c473642d4c8da54a93db3aed40ea2742fcfb86c8
GibinJacob <gibin.jacob.job@gmail.com>
2018-03-20 10:01:06 +0530

    headerfiles missing added

--- ProductionApp.BusinessService/Services/CustomerInvoiceBusiness.cs
+++ ProductionApp.BusinessService/Services/CustomerInvoiceBusiness.cs
@@ -1,4 +1,5 @@
 ﻿using ProductionApp.BusinessService.Contracts;
+using ProductionApp.RepositoryServices.Contracts;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- ProductionApp.UserInterface/Controllers/CustomerInvoiceController.cs
+++ ProductionApp.UserInterface/Controllers/CustomerInvoiceController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ProductionApp.BusinessService.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
