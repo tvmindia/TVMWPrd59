71fcd819ad269d9217b93f5ee34c3670a399053c
Angel Paul <Angel Paul@DESKTOP-DCRUNKI>
2018-03-21 14:44:38 +0530

    files include in project.

--- ProductionApp.UserInterface/Controllers/MaterialReturnController.cs
+++ ProductionApp.UserInterface/Controllers/MaterialReturnController.cs
@@ -1,5 +1,6 @@
 ﻿using ProductionApp.BusinessService.Contracts;
 using ProductionApp.DataAccessObject.DTO;
+using ProductionApp.UserInterface.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,10 +22,89 @@ namespace ProductionApp.UserInterface.Controllers
             ViewBag.SysModuleCode = code;
             return View();
         }
+
         public ActionResult NewMaterialReturn(string code)
         {
             ViewBag.SysModuleCode = code;
             return View();
         }
+
+        #region ButtonStyling
+        [HttpGet]
+        public ActionResult ChangeButtonStyle(string actionType)
+        {
+            ToolboxViewModel toolboxVM = new ToolboxViewModel();
+            switch (actionType)
+            {
+                case "List":
+                    toolboxVM.addbtn.Visible = true;
+                    toolboxVM.addbtn.Text = "Add";
+                    toolboxVM.addbtn.Title = "Add New";
+                    toolboxVM.addbtn.Href = Url.Action("NewMaterialReturn", "MaterialReturn", new { code = "STR" });
+                    //----added for reset button---------------
+                    toolboxVM.resetbtn.Visible = true;
+                    toolboxVM.resetbtn.Text = "Reset";
+                    toolboxVM.resetbtn.Title = "Reset All";
+                    toolboxVM.resetbtn.Event = "ResetPackingSlipList();";
+                    //----added for export button--------------
+                    toolboxVM.PrintBtn.Visible = true;
+                    toolboxVM.PrintBtn.Text = "Export";
+                    toolboxVM.PrintBtn.Title = "Export";
+                    toolboxVM.PrintBtn.Event = "ImportPackingSlipData();";
+                    //---------------------------------------
+
+                    break;
+                case "Edit":
+
+                    toolboxVM.addbtn.Visible = true;
+                    toolboxVM.addbtn.Text = "New";
+                    toolboxVM.addbtn.Title = "Add New";
+                    toolboxVM.addbtn.Href = Url.Action("NewMaterialReturn", "MaterialReturn", new { code = "STR" });
+
+                    toolboxVM.savebtn.Visible = true;
+                    toolboxVM.savebtn.Text = "Save";
+                    toolboxVM.savebtn.Title = "Save";
+                    toolboxVM.savebtn.Event = "Save();";
+
+                    toolboxVM.deletebtn.Visible = true;
+                    toolboxVM.deletebtn.Text = "Delete";
+                    toolboxVM.deletebtn.Title = "Delete";
+                    toolboxVM.deletebtn.Event = "DeleteClick()";
+
+                    toolboxVM.resetbtn.Visible = true;
+                    toolboxVM.resetbtn.Text = "Reset";
+                    toolboxVM.resetbtn.Title = "Reset";
+                    toolboxVM.resetbtn.Event = "Reset();";
+
+                    toolboxVM.ListBtn.Visible = true;
+                    toolboxVM.ListBtn.Text = "List";
+                    toolboxVM.ListBtn.Title = "List";
+                    toolboxVM.ListBtn.Event = "";
+                    toolboxVM.ListBtn.Href = Url.Action("ViewMaterialReturn", "MaterialReturn", new { Code = "STR" });
+
+
+                    break;
+                case "Add":
+
+                    toolboxVM.savebtn.Visible = true;
+                    toolboxVM.savebtn.Text = "Save";
+                    toolboxVM.savebtn.Title = "Save";
+                    toolboxVM.savebtn.Event = "Save();";
+
+                    toolboxVM.ListBtn.Visible = true;
+                    toolboxVM.ListBtn.Text = "List";
+                    toolboxVM.ListBtn.Title = "List";
+                    toolboxVM.ListBtn.Event = "";
+                    toolboxVM.ListBtn.Href = Url.Action("ViewMaterialReturn", "MaterialReturn", new { Code = "STR" });
+
+
+                    break;
+                default:
+                    return Content("Nochange");
+            }
+            return PartialView("ToolboxView", toolboxVM);
+        }
+
+        #endregion
     }
 }
