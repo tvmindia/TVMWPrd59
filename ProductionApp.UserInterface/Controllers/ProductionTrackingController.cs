3bd3e8945f325fc092789529f3d2222f47533a59
Arul Vyas <Arul Vyas@DESKTOP-G0M3RCR>
2018-04-03 17:47:51 +0530

    ProductionTracking

---
+++ ProductionApp.UserInterface/Controllers/ProductionTrackingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductionApp.UserInterface.Controllers
+{
+    public class ProductionTrackingController : Controller
+    {
+        // GET: ProductionTracking
+        public ActionResult NewProductionTracking()
+        {
+            return View();
+        }
+    }
+}
