2989a7cdb6603cbbcdae7699f6edf14034f6e49a
Jais <jais.thrithvam@outlook.com>
2018-04-11 16:42:21 +0530

    1.Modifications in masters inamc

--- ProductionApp.BusinessService/Contracts/IDocumentTypeBusiness.cs
+++ ProductionApp.BusinessService/Contracts/IDocumentTypeBusiness.cs
@@ -4,11 +4,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace ProductionApp.BusinessService.Contracts
 {
     public interface IDocumentTypeBusiness
     {
-        List<DocumentType> GetDocumentTypeForSelectList();
+        List<SelectListItem> GetDocumentTypeForSelectList();
     }
 }
