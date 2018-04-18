f04cb7432f61b603bae91c4c546566538cf78ab5
GibinJacob <gibin.jacob.job@gmail.com>
2018-04-18 07:09:58 +0530

    DTO Properties

--- ProductionApp.DataAccessObject/DTO/SupplierPayment.cs
+++ ProductionApp.DataAccessObject/DTO/SupplierPayment.cs
@@ -8,9 +8,50 @@ namespace ProductionApp.DataAccessObject.DTO
 {
     public class SupplierPayment
     {
+        public Guid ID { get; set; }
+        public string EntryNo { get; set; }
+        public Guid? SupplierID { get; set; }
+        public string PaymentMode { get; set; }
+        public Guid? DepositWithdrawalID { get; set; }
+        public string BankCode { get; set; }
+        public string PaymentRef { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string GeneralNotes { get; set; }
+        public decimal? TotalPaidAmt { get; set; }
+        public Guid? CreditID { get; set; }
+        public string Type { get; set; }
+        public DateTime? ChequeDate { get; set; }
+        public string ReferenceBank { get; set; }
+
+        public Guid LatestApprovalID { get; set; }
+        public int LatestApprovalStatus { get; set; }
+        public string ApprovalStatus { get; set; }
+
+        public bool IsFinalApproved { get; set; }
+
+        //Additional properties
+        public Common Common { get; set; }
+        public string PaymentDateFormatted { get; set; }
+        public string ChequeDateFormatted { get; set; }
+        public SupplierPaymentDetail SupplierPaymentDetail { get; set; }
+        public List<SupplierPaymentDetail> SupplierPaymentDetailList { get; set; }
+        public string SupplierName { get; set; }
+        public decimal? AdvanceAmount { get; set; }
+        public string CreditNo { get; set; }
+        public string DetailXML { get; set; }
+        public bool IsUpdate { get; set; }
+        public string DetailJSON { get; set; }
+        public Guid hdnFileID { get; set; }
+        public int TotalCount { get; set; }
+        public int FilteredCount { get; set; }
     }
     public class SupplierPaymentDetail
     {
+        public Guid ID { get; set; }
+        public Guid PaymentID { get; set; }
+        public Guid InvoiceID { get; set; }
+        public decimal PaidAmount { get; set; }
+
     }
     public class SupplierPaymentAdvanceSearch
     {
--- ProductionApp.UserInterface/Controllers/SupplierPaymentController.cs
+++ ProductionApp.UserInterface/Controllers/SupplierPaymentController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using ProductionApp.BusinessService.Contracts;
+using ProductionApp.DataAccessObject.DTO;
+using ProductionApp.UserInterface.Models;
+using ProductionApp.UserInterface.SecurityFilter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,14 +13,75 @@ namespace ProductionApp.UserInterface.Controllers
 {
     public class SupplierPaymentController : Controller
     {
-        // GET: SupplierPayment
-        public ActionResult ViewSupplierPayment()
+        private ISupplierPaymentBusiness _supplierPaymentBusiness;
+        private ISupplierBusiness _supplierBusiness;
+      //  private IChartOfAccountBusiness _chartOfAccountBusiness;
+      //  private IPaymentTermBusiness _paymentTermBusiness;
+      //  private IPurchaseOrderBusiness _purchaseOrderBusiness;
+
+        Common _common = new Common();
+        AppConst _appConst = new AppConst();
+
+        public SupplierPaymentController(ISupplierPaymentBusiness supplierPaymentBusiness,
+            ISupplierBusiness supplierBusiness)
+            //, IChartOfAccountBusiness chartOfAccountBusiness, 
+            //IPaymentTermBusiness paymentTermBusiness, IPurchaseOrderBusiness purchaseOrderBusiness)
         {
-            return View();
+            _supplierPaymentBusiness = supplierPaymentBusiness;
+            _supplierBusiness = supplierBusiness;
+            //_chartOfAccountBusiness = chartOfAccountBusiness;
+            //_paymentTermBusiness = paymentTermBusiness;
+            //_purchaseOrderBusiness = purchaseOrderBusiness;
         }
-        public ActionResult NewSupplierPayment()
+        // GET: SupplierPayment
+        [AuthSecurityFilter(ProjectObject = "SupplierPayment", Mode = "R")]
+        public ActionResult ViewSupplierPayment(string code)
         {
-            return View();
+            ViewBag.SysModuleCode = code;
+            SupplierPaymentAdvanceSearchViewModel supplierPaymentAdvanceSearchVM = new SupplierPaymentAdvanceSearchViewModel();
+            supplierPaymentAdvanceSearchVM.Supplier = new SupplierViewModel();
+            List<SelectListItem> selectListItem = new List<SelectListItem>();
+            supplierPaymentAdvanceSearchVM.Supplier.SelectList = new List<SelectListItem>();
+            List<SupplierViewModel> supplierList = Mapper.Map<List<Supplier>, List<SupplierViewModel>>(_supplierBusiness.GetSupplierForSelectList());
+            if (supplierList != null)
+                foreach (SupplierViewModel supplier in supplierList)
+                {
+                    selectListItem.Add(new SelectListItem
+                    {
+                        Text = supplier.CompanyName,
+                        Value = supplier.ID.ToString(),
+                        Selected = false
+                    });
+                }
+            supplierPaymentAdvanceSearchVM.Supplier.SelectList = selectListItem;
+            return View(supplierPaymentAdvanceSearchVM);
+        }
+        [AuthSecurityFilter(ProjectObject = "SupplierPayment", Mode = "R")]
+        public ActionResult NewSupplierPayment(string code, Guid? id)
+        {
+            ViewBag.SysModuleCode = code;
+            SupplierPaymentViewModel supplierPaymentVM = new SupplierPaymentViewModel
+            {
+                ID = id == null ? Guid.Empty : (Guid)id,
+                IsUpdate = id == null ? false : true,
+            };
+            supplierPaymentVM.Supplier = new SupplierViewModel();
+            List<SelectListItem> selectListItem = new List<SelectListItem>();
+            supplierPaymentVM.Supplier.SelectList = new List<SelectListItem>();
+            List<SupplierViewModel> supplierList = Mapper.Map<List<Supplier>, List<SupplierViewModel>>(_supplierBusiness.GetSupplierForSelectList());
+            if (supplierList != null)
+                foreach (SupplierViewModel supplier in supplierList)
+                {
+                    selectListItem.Add(new SelectListItem
+                    {
+                        Text = supplier.CompanyName,
+                        Value = supplier.ID.ToString(),
+                        Selected = false
+                    });
+                }
+            supplierPaymentVM.Supplier.SelectList = selectListItem;
+            return View(supplierPaymentVM);
+
         }
     }
 }
