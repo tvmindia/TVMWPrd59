28c9825a1c2216e98b964fbb9e72e710af541f9b
GibinJacob <gibin.jacob.job@gmail.com>
2018-04-18 07:19:44 +0530

    JS code

--- ProductionApp.UserInterface/Scripts/UserJS/SupplierPayment/ViewSupplierPayment.js
+++ ProductionApp.UserInterface/Scripts/UserJS/SupplierPayment/ViewSupplierPayment.js
@@ -1,1 +1,35 @@
-﻿
+﻿//*****************************************************************************
+//*****************************************************************************
+//Author: Gibin Jacob
+//CreatedDate: 18-Apr-2018 
+//LastModified:  18-Apr-2018 
+//FileName: NewSupplierPayment.js
+//Description: Client side coding for New/Edit Supplier Payment
+//******************************************************************************
+// ##1--Global Declaration
+// ##2--Document Ready function
+// ##3-- 
+// ##4-- 
+// ##5-- 
+// ##6-- 
+// ##7-- 
+// ##8-- 
+// ##9-- 
+// ##10-- 
+// ##11--Save  Supplier Payment 
+// ##12--Save Success Supplier Payment
+// ##13--Bind Supplier Payment By ID
+// ##14--Reset Button Click
+// ##15-- 
+// ##16--DELETE Supplier Payment 
+// ##17--DELETE Supplier Payment Details 
+// 
+//******************************************************************************
+
+//##1--Global Declaration---------------------------------------------##1 
+var _DataTables = {};
+var EmptyGuid = "00000000-0000-0000-0000-000000000000";
+var _SlNo = 1;
+var _result = "";
+var _message = "";
+var _jsonData = {};
