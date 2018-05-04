577f49a20e4e3d27e4d41e673ebf473d36747927
Angel Paul <Angel Paul@DESKTOP-DCRUNKI>
2018-05-04 10:43:58 +0530

    Approval Status Dropdown- New Files are Added.

---
+++ ProductionApp.UserInterface/Controllers/ApprovalStatusController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductionApp.UserInterface.Controllers
+{
+    public class ApprovalStatusController : Controller
+    {
+        // GET: ApprovalStatus
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
